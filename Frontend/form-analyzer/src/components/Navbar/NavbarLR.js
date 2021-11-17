ae6e974731377b575a972d5069c65086acb8a8e5
Vishal Salgond <vnsalgond@gmail.com>
2021-11-17 23:15:32 +0530

    registration changed

--- Frontend/form-analyzer/src/components/Navbar/NavbarLR.js
+++ Frontend/form-analyzer/src/components/Navbar/NavbarLR.js
@@ -3,13 +3,17 @@ import React from 'react'
 const NavbarLR = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Form Analyzer</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-            </div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light p-4">
+                <div class="container">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">Form Data Analyzer</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+
+                </div>
+
             </nav>
         </div>
     )
