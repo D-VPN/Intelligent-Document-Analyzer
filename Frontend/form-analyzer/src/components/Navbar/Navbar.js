f75d522d249189ac311da72c786104a131ac4017
darshansatra1 <darshansatra1@gmail.com>
2021-11-17 23:36:13 +0530

    navbar redesign

--- Frontend/form-analyzer/src/components/Navbar/Navbar.js
+++ Frontend/form-analyzer/src/components/Navbar/Navbar.js
@@ -3,17 +3,20 @@ import React from 'react'
 const NavbarLR = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Form Analyzer</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0"> </ul> {/* This is for keeping below code at RHS of navbar */}
-                        <a><span class='text-light'> UserName </span></a>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">Form Data Analyzer</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0"> </ul> {/* This is for keeping below code at RHS of navbar */}
+                            <a><span class='text-light'> UserName </span></a>
+                        </div>
                     </div>
                 </div>
+
             </nav>
         </div>
     )
