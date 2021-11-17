7161de5906d6cfabc307ce93e16eb97985bde32f
Vishal Salgond <vnsalgond@gmail.com>
2021-11-17 23:18:28 +0530

    dashboard changes

--- Frontend/form-analyzer/src/components/Dashboard/Dashboard.js
+++ Frontend/form-analyzer/src/components/Dashboard/Dashboard.js
@@ -6,7 +6,32 @@ const Dashboard = ({ setToken }) => {
     return (
         <div>
             <Navbar />
-            This is a dashboard
+            <div class='body'>
+                <div class='row'>
+                    <div class='col-md-4'>
+                        <div class="card shadow text-center" style={{width: "18rem"}}>
+                            <div class="card-body">
+                                <h5 class="card-header">Have a New Form?</h5>
+                                <div class='card-textarea'>
+                                    <a href='#' class=' card-link'><p class="card-text">CREATE PROJECT</p></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='col-md-4'>
+                        <div class="card shadow text-center" style={{width: "18rem"}}>
+                            <div class="card-body">
+                                <h5 class="card-header">PRODUCT SURVEY</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Created 4 days ago...</h6>
+                                <div class='card-textarea'>
+                                    <button class='btn btn-primary'>View Project</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
--- Frontend/form-analyzer/src/components/Login/Login.js
+++ Frontend/form-analyzer/src/components/Login/Login.js
@@ -23,24 +23,27 @@ const Login = ({ setToken }) => {
             return;
         }
         const url = "http://127.0.0.1:8000/auth/token/login";
-        await axios.post(url, {
-            email: email,
-            password: password,
-        },
-            {
-                'Content-Type': 'application/json',
-            }
-        ).then((response) => {
-            if (response.status === 200) {
-                const user = {
-                    email: email,
-                    token: response.data.auth_token,
-                };
-                setToken(user);
-                navigate('/', { replace: true, });
-            }
-
-        }).catch((error) => {
+        try {
+            const response = await axios.post(url, {
+                email: email,
+                password: password,
+            },
+                {
+                    'Content-Type': 'application/json',
+                }
+            );
+            const userUrl = "http://127.0.0.1:8000/auth/users/me/";
+            const { data } = await axios.get(userUrl, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Token ${response.data.auth_token}`
+                }
+            });
+            data.token = response.data.auth_token;
+            setToken(data);
+            navigate('/', { replace: true, });
+        }
+        catch (error) {
             if (error.response) {
                 if (error.response.data.email) {
                     setError(error.response.data.email[0]);
@@ -53,7 +56,8 @@ const Login = ({ setToken }) => {
 
                 }
             }
-        });
+        }
+
     }
     const showError = () => {
         return (
@@ -61,52 +65,54 @@ const Login = ({ setToken }) => {
         )
     }
     return (
+        <div>
+            <NavbarLR />
 
-        <div className='container login-container mt-5'>
-            <div class="card mb-5 shadow " style={{ maxWidth: "800px", margin: "auto", borderRadius: "2%" }}>
-                <div class="row g-0">
-                    <div class="col-md-6">
-                        <img src={login} style={{ height: "100%", width: "100%", borderRadius: "2%" }} />
-                    </div>
-                    <div class="col-md-6 p-5">
-                        <h1 className='text-center login-header mb-5'>LOGIN</h1>
-                        <form onSubmit={(e) => onLogin(e)}>
-                            <div class="mb-3">
-                                <label for="email" class="form-label">Email</label>
-                                <input
-                                    type="email"
-                                    class="form-control"
-                                    id="email"
-                                    placeholder="name@example.com"
-                                    value={email} onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </div>
-                            <div class="mb-3">
-                                <label for="pw" class="form-label">Password</label>
-                                <input
-                                    type="password"
-                                    class="form-control"
-                                    id="pw"
-                                    value={password} onChange={(e) => setPassword(e.target.value)}
-                                />
-                            </div>
-                            {showError()}
-                            <div class="d-grid gap-2 mt-5">
-                                <button className="submit__btn" type='submit'>Submit</button>
-                            </div>
-                            <div class='row mt-3'>
-                                <Link to="/registration" replace>
-                                    <a href="">Don't have an Account? Register</a>
-                                </Link>
-                            </div>
+            <div className='container login-container mt-5'>
+                <div class="card mb-5 shadow " style={{ maxWidth: "800px", margin: "auto", borderRadius: "2%" }}>
+                    <div class="row g-0">
+                        <div class="col-md-6">
+                            <img src={login} style={{ height: "100%", width: "100%", borderRadius: "2%" }} />
+                        </div>
+                        <div class="col-md-6 p-5">
+                            <h1 className='text-center login-header mb-5'>LOGIN</h1>
+                            <form onSubmit={(e) => onLogin(e)}>
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">Email</label>
+                                    <input
+                                        type="email"
+                                        class="form-control"
+                                        id="email"
+                                        placeholder="name@example.com"
+                                        value={email} onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                </div>
+                                <div class="mb-3">
+                                    <label for="pw" class="form-label">Password</label>
+                                    <input
+                                        type="password"
+                                        class="form-control"
+                                        id="pw"
+                                        value={password} onChange={(e) => setPassword(e.target.value)}
+                                    />
+                                </div>
+                                {showError()}
+                                <div class="d-grid gap-2 mt-5">
+                                    <button className="submit__btn" type='submit'>Submit</button>
+                                </div>
+                                <div class='row mt-3'>
+                                    <Link to="/registration" replace>
+                                        <a href="">Don't have an Account? Register</a>
+                                    </Link>
+                                </div>
 
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
 
 
-            {/* <div className='row login-row'>
+                {/* <div className='row login-row'>
         <div>
             <NavbarLR></NavbarLR>
             <div className='container login-container'>
@@ -144,31 +150,52 @@ const Login = ({ setToken }) => {
                         <div class='row mt-3'>
                             <div class='col-md-6 mx-auto'>
 
-                                <Link to="/registration" replace>
-                                    <a href="">Don't have an Account? Register</a>
-                                </Link>
+                <h1 className='text-center login-header'>LOGIN</h1>
+                <div className='row login-row'>
+                    <div className='col-md-6'>
+                        <img src={login} alt="Logo" style={{ height: "90%", width: "100%" }} className='img-fluid' />
+                    </div>
+                    <div className='col-md-6'>
+                        <form onSubmit={(e) => onLogin(e)}>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email:</label>
+                                <input
+                                    type="email"
+                                    class="form-control"
+                                    id="email"
+                                    placeholder="name@example.com"
+                                    value={email} onChange={(e) => setEmail(e.target.value)}
+                                />
                             </div>
-                        </div>
+                            <div class="mb-3">
+                                <label for="pw" class="form-label">Password:</label>
+                                <input
+                                    type="password"
+                                    class="form-control"
+                                    id="pw"
+                                    value={password} onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </div>
+                            {showError()}
+                            <div class="d-grid gap-2">
+                                <button className="submit__btn" type='submit'>Submit</button>
+                            </div>
+                            <div class='row mt-3'>
+                                <div class='col-md-6 mx-auto'>
 
-                    </form>
+                                    <Link to="/registration" replace>
+                                        <a href="">Don't have an Account? Register</a>
+                                    </Link>
+                                </div>
+                            </div>
 
+                        </form>
+
+                    </div>
                 </div>
             </div> */}
-        </div >
-
-
-        /* <div className="login">
-            <form className="login__form" onSubmit={(e) => onLogin(e)}>
-                <h1>
-                    Login Here
-                </h1>
-                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                {showError()}
-                <button type="submit" className="submit__btn">Submit</button>
-
-            </form> */
-
+            </div >
+        </div>
     )
 }
 
