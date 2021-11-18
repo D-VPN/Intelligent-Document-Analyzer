1ebc40e35f5021d0ee7f018f15a17342e4b11e69
Param Shendekar <paramvs8@gmail.com>
2021-11-18 11:34:19 +0530

    Navbar small changes

--- Frontend/form-analyzer/src/components/Navbar/Navbar.js
+++ Frontend/form-analyzer/src/components/Navbar/Navbar.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import './Navbar.css';
 
-const NavbarLR = () => {
+const Navbar = ({logout}) => {
     return (
-        <div>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div >
+            <nav class="navbar navbar-expand-lg bg-info" className='navbar-color'>
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Form Analyzer</a>
+                <a class="navbar-brand text-dark" href="#">Form Data Analyzer</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0"> </ul> {/* This is for keeping below code at RHS of navbar */}
-                <a><span class='text-light'> UserName </span></a>
+                {/* <a><span class='text-light'> UserName </span></a> */}
+                <button class='btn btn-primary bg-dark' onClick = {(e) => logout()} >Log Out</button>
                 </div>
             </div>
             </nav>
@@ -19,4 +21,4 @@ const NavbarLR = () => {
     )
 }
 
-export default NavbarLR;
+export default Navbar;
