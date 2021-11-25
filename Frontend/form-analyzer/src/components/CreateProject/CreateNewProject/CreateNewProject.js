96ffe4a9811aec6c6a0c55d1343414897976c859
Param Shendekar <paramvs8@gmail.com>
2021-11-25 12:01:23 +0530

    Create New Project Form FrontEnd

--- Frontend/form-analyzer/src/components/CreateProject/CreateNewProject/CreateNewProject.js
+++ Frontend/form-analyzer/src/components/CreateProject/CreateNewProject/CreateNewProject.js
@@ -1,25 +1,33 @@
 import React from 'react'
-import NavbarLR from '../../Navbar/NavbarLR';
+import Navbar from '../../Navbar/Navbar';
+import './CreateNewProject.css';
 
 const CreateNewProject = () => {
     return (
         <div>
-            <NavbarLR/>
+            <Navbar/>
             <div class='container'>
-                <div class='header'>
+                <div class='header text-center'>
                     <h1>Create New Project</h1>
                 </div>
-                <div>
+                <div class='form-container row'>
+                    <div class='col-md-3'></div>
+                    <div class='col-md-6'>
                     <form>
-                        <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="floatingInput" placeholder="My Project"/>
+                        <div class=" mb-3">
                             <label for="floatingInput">Project Name</label>
+                            <input type="text" class="form-control" id="floatingInput" placeholder="My Project"/>
                         </div>
-                        <div class="form">
+                        <div class="upload">
                             <input type="file" class="form-control" id="floatingPassword" placeholder="Password"/>
                             <label for="floatingPassword">Upload Template Form</label>
                         </div>
+                        <div class="d-grid gap-2 mt-5">
+                            <button className="submit__btn" type='submit'>NEXT</button>
+                        </div>
                     </form>
+                    </div>
+                    
                 </div>
             </div>
         </div>
