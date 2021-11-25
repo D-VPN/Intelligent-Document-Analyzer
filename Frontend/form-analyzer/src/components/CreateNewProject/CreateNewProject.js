0dabbc890ca83aa817485042ae080c7608e965bb
Param Shendekar <paramvs8@gmail.com>
2021-11-25 11:19:51 +0530

    Create New Project initialized

---
+++ Frontend/form-analyzer/src/components/CreateNewProject/CreateNewProject.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateNewProject = () => {
+    return (
+        <div>
+            <h1>This is Create New Project Page</h1>
+        </div>
+    )
+}
+
+export default CreateNewProject
