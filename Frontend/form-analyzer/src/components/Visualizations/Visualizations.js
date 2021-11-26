a138b31ac871120506800fb67ab953b7b0f61515
Param Shendekar <paramvs8@gmail.com>
2021-11-26 21:59:51 +0530

    Last 2 screens initiated

---
+++ Frontend/form-analyzer/src/components/Visualizations/Visualizations.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar';
+
+const Visualizations = () => {
+    return (
+        <div>
+            <Navbar/>
+            <div class='container'>
+                <div class='header text-center'>
+                    <h1>Project Name Dynamically Render Karna</h1>
+                    <p><small>69 Forms Added</small></p>
+                </div>
+                <div class='row'>
+                    <div class='visualization-box'>
+                        Yahape Visualizations Aayenge
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Visualizations;
