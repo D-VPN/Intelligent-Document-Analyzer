439b6f9a8aa7e3393212cb8efd329081571f5c14
Param Shendekar <paramvs8@gmail.com>
2021-11-27 12:47:39 +0530

    All Screens Basic. Chart.js Init

--- Frontend/form-analyzer/src/components/CreateProject/UploadForms/UploadForms.js
+++ Frontend/form-analyzer/src/components/CreateProject/UploadForms/UploadForms.js
@@ -11,10 +11,12 @@ const UploadForms = ({ nextStep, values, prevStep }) => {
                 <div class='row'>
                     <div class='col-md-3 '></div>
                     <div class='col-md-6 box'>
-                        <div class="mb-3">
-                            <label for="bulkForms" class="form-label">Upload Your Forms</label>
-                            <input class="form-control form-control-lg" type="file" id="bulkForms" multiple />
-                        </div>
+                        <form>
+                            <div class="mb-3">
+                                <label for="bulkForms" class="form-label">Upload Your Forms</label>
+                                <input class="form-control form-control-lg" type="file" id="bulkForms" multiple />
+                            </div>
+                        </form>
                         <div class="row mt-5">
                             <div class='d-grid col-md-6'>
                                 <button className="submit__btn" type='submit' onClick={prevStep} >PREVIOUS</button>
