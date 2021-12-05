cd72317dcaedd13dc3c5059a2b3cf8889345e584
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2021-12-05 13:03:40 +0530

    fix missing import

--- Frontend/form-analyzer/src/components/Dashboard/Dashboard.js
+++ Frontend/form-analyzer/src/components/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // import './Dashboard.css';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
