66b07c9918c8ac6363f9981fd7454c6c8221b60f
darshansatra1 <darshansatra1@gmail.com>
2021-12-05 13:17:13 +0530

    update axios

--- Frontend/form-analyzer/src/helper/axios.js
+++ Frontend/form-analyzer/src/helper/axios.js
@@ -2,11 +2,17 @@ import axios from "axios";
 const authToken = () => {
     const userDetail = sessionStorage.getItem('user');
     const user = JSON.parse(userDetail);
+    console.log(user)
     return user?.token
 }
 const instance = axios.create({
     baseURL: "http://127.0.0.1:8000",
 })
-instance.defaults.headers.common["Authorization"] = `Token ${authToken()}`;
+instance.interceptors.request.use((config) => {
+    console.log("DARSHAN")
+    config.headers.Authorization = `Token ${authToken()}`;
+    return config;
+})
 instance.defaults.headers.common["Content-Type"] = `application/json`;
+
 export default instance;
