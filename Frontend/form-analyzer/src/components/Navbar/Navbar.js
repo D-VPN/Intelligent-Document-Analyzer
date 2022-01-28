2720e4063070defe02271c765d7d304cbe0a19e3
darshansatra1 <darshansatra1@gmail.com>
2022-01-28 13:17:25 +0530

    add username in Navbar

--- Frontend/form-analyzer/src/components/Navbar/Navbar.js
+++ Frontend/form-analyzer/src/components/Navbar/Navbar.js
@@ -13,13 +13,26 @@ class Navbar extends Component {
         this.props.setToken();
         this.props.navigate("/login", { replace: true });
     }
+
+    getUser = () => {
+        const userDetail = sessionStorage.getItem('user');
+        const user = JSON.parse(userDetail);
+        return <div class="text-light mx-5 fs-4" style={{
+            "display": "inline-flex",
+            "align-items": "center"
+        }}>
+            <img class="mx-2" src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/32/000000/external-user-interface-kiranshastry-gradient-kiranshastry-1.png" />
+            {user.username}
+        </div>;
+    }
+
     render() {
 
         const body = this.props.token ?
             <div>
                 < nav class="navbar navbar-expand-lg bg-dark px-3" >
                     <div class="container-fluid">
-                        <div class="d-flex pt-3">
+                        <div class="d-flex pt-3" >
                             <img src="logo.png" height={45} width={45} />
                             <Link to="/">
                                 <a class="navbar-brand text-light" href="#"> <h2 class="mx-2">Form Data Analyzer</h2></a>
@@ -29,8 +42,9 @@ class Navbar extends Component {
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto mb-lg-0"> </ul> {/* This is for keeping below code at RHS of navbar */}
-                            {/* <a><span class='text-light'> UserName </span></a> */}
+                            <ul class="navbar-nav me-auto mb-lg-0"> </ul>
+
+                            {this.getUser()}
                             <button class='btn btn-primary' onClick={(e) => this.logout()} >Log Out</button>
                         </div>
                     </div>
