10ae59e09ad6e5dc9de44f352629cd8f22f6e01e
darshansatra1 <darshansatra1@gmail.com>
2022-01-30 19:01:30 +0530

    get project data from api

--- Frontend/form-analyzer/src/components/Project/Visualizations.js
+++ Frontend/form-analyzer/src/components/Project/Visualizations.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -9,7 +9,8 @@ import {
     Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
-
+import axios from '../../helper/axios';
+import { useParams } from 'react-router-dom';
 
 ChartJS.register(
     CategoryScale,
@@ -20,48 +21,68 @@ ChartJS.register(
     Legend
 );
 
-export default class Visualizations extends React.Component {
-    constructor(props) {
-        super(props)
-        console.log(props);
-    }
+const Visualizations = () => {
+    const [loading, setLoading] = useState(true)
+    const [project, setProject] = useState({
+        name: null,
+        createdAt: null,
+        formNumber: null,
+        keys: [],
+    });
+    var projectId = useParams().projectId;
 
-
-    render() {
-        return (
-            <div>
-                <div class='container'>
-                    <div class='header text-center'>
-                        <h1>Your Data Insights</h1>
-                        <p><small>5 Forms Added</small></p>
-                    </div>
-                    <div class='row'>
-                        {/* <div class='visualization-box'>
+    useEffect(async () => {
+        try {
+            const { data } = await axios.post('/project-dashboard/', {
+                project_id: projectId,
+            });
+            setProject(prevState => ({
+                ...prevState,
+                name: data.name,
+                createdAt: new Date(data.created_at),
+                formNumber: data.number_of_forms,
+                keys: data.keys,
+            }));
+        } catch (e) {
+            console.error(e);
+        }
+    }, []);
+    console.log(project);
+    return (
+        <div>
+            <div class='container'>
+                <div class='header text-center'>
+                    <h1>Your Data Insights</h1>
+                    <p><small>5 Forms Added</small></p>
+                </div>
+                <div class='row'>
+                    {/* <div class='visualization-box'>
                         Yahape Visualizations Aayenge
                     </div>
                     <div>
                         <canvas id="myChart"></canvas>
                     </div> */}
-                        <Bar
-                            height={100}
-                            width={300}
-                            options={{
-                                responsive: true,
-                            }}
-                            data={{
-                                labels: ['Male', 'Female'],
-                                datasets: [{
-                                    label: '# of Employees',
-                                    data: [2, 3],
-                                    backgroundColor: [
+                    <Bar
+                        height={100}
+                        width={300}
+                        options={{
+                            responsive: true,
+                        }}
+                        data={{
+                            labels: ['Male', 'Female'],
+                            datasets: [{
+                                label: '# of Employees',
+                                data: [2, 3],
+                                backgroundColor: [
 
-                                    ]
-                                }]
-                            }}
-                        />
-                    </div>
+                                ]
+                            }]
+                        }}
+                    />
                 </div>
             </div>
-        );
-    }
-} 
+        </div>
+    )
+}
+
+export default Visualizations;
