ac2072e344a7a8c88793e0b71f8414e287ef2ff0
darshansatra1 <darshansatra1@gmail.com>
2022-02-20 15:51:38 +0530

    implement csv data download

---
+++ Frontend/form-analyzer/src/helper/content_axios.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+const authToken = () => {
+    const userDetail = localStorage.getItem('user');
+    const user = JSON.parse(userDetail);
+    return user?.token
+}
+const instance = axios.create({
+    baseURL: "http://127.0.0.1:8000",
+})
+instance.interceptors.request.use((config) => {
+    config.headers.Authorization = `Token ${authToken()}`;
+    return config;
+})
+instance.defaults.headers.common["Content-Type"] = `text/csv`;
+
+export default instance;
